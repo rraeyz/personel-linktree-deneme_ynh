1073aa5c318fae718afe5bfc4d0240aff1be9024
rraeyz <rraeyz@users.noreply.github.com>
2026-02-18 03:14:23 +0300

    fix: Add outputFileTracingIncludes for missing runtime dependencies in standalone build

--- next.config.js
+++ next.config.js
@@ -9,6 +9,18 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/': [
+        './node_modules/bcryptjs/**/*',
+        './node_modules/jsonwebtoken/**/*',
+        './node_modules/nodemailer/**/*',
+        './node_modules/geoip-lite/**/*',
+        './node_modules/ua-parser-js/**/*',
+        './node_modules/qrcode.react/**/*',
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
